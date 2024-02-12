86aea657ded651109f3ffb4be6e44fe9e0abefda
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-12 10:06:11 +0100

    adding route protrction

--- src/util/auth.js
+++ src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom";
+
 export default function getAuthToken() {
     const token = localStorage.get("token");
     return token;
@@ -5,4 +7,14 @@ export default function getAuthToken() {
 
 export function tokenLoader() {
     return getAuthToken();
+}
+
+export function checkAuthLoader() {            //adding protection to route
+    const token = getAuthToken();
+
+    if(!token) {
+        return redirect("/auth");
+    }
+
+    return null;
 }
