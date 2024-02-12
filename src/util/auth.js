6657bc3c617aadef4e36ee7a6e13756ba8a8ddfd
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-12 11:37:33 +0100

    managing token expiration

--- src/util/auth.js
+++ src/util/auth.js
@@ -1,7 +1,26 @@
 import { redirect } from "react-router-dom";
 
+export function getTokenDuration() {
+    const storedExpirationDate = localStorage.getItem("expiration");
+    const expirationDate = new Date(storedExpirationDate);
+    const now = new Date();
+    const duration = expirationDate.getTime() - now.getTime();
+    return duration;
+}
+
 export default function getAuthToken() {
-    const token = localStorage.get("token");
+    const token = localStorage.getItem("token");
+
+    if(!token) {
+        return null;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    if(tokenDuration < 0) {
+        return "EXPIRED";
+    }
+
     return token;
 }
 
